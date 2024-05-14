8e171c02c2663dc62e5e0f8cdcfab3d70693b85c
Os <ostap.basarab.job@gmail.com>
2024-05-14 17:28:27 +0300

    create article

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { ArticleService } from "./article.service";
+import { AuthGuard } from "src/guards/auth.guard";
+import { UserEntity } from "src/user/user.entity";
+import { User } from "src/decorators/user.decorator";
+import { CreateArticleDto } from "./dto/article.dto";
+import { ArticleResponseInterface } from "./types/articleResponse.interface";
 
 @Controller('articles')
 export class ArticleController {
     constructor(private readonly articleService: ArticleService) {}
 
     @Post()
-    async create() {
-        return this.articleService.createArticle();
+    @UseGuards(AuthGuard)
+    async create(@User() currentUser: UserEntity, @Body('article') createArticleDto:CreateArticleDto): Promise<ArticleResponseInterface> {
+        const article = await this.articleService.createArticle(currentUser, createArticleDto);
+        return this.articleService.buildArticleResponse(article);
     }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from "@nestjs/common";
+import { UserEntity } from "src/user/user.entity";
+import { CreateArticleDto } from "./dto/article.dto";
+import { ArticleEntity } from "./article.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ArticleResponseInterface } from "./types/articleResponse.interface";
+import slugify from "slugify";
 
 @Injectable()
 export class ArticleService {
-    async createArticle() {
-        return 'create article in service'
+    constructor(
+        @InjectRepository(ArticleEntity)
+         private readonly articleRepository: Repository<ArticleEntity>
+        ) {}
+    async createArticle(currentUser: UserEntity, createArticleDto: CreateArticleDto): Promise<ArticleEntity> {
+        const article = new ArticleEntity()
+        Object.assign(article, createArticleDto)
+
+        if(!article.tagList) article.tagList = [];
+
+        article.slug = this.getSlug(createArticleDto.title)
+
+        article.author = currentUser;
+
+        return await this.articleRepository.save(article)
+    }
+
+    buildArticleResponse(article: ArticleEntity): ArticleResponseInterface {
+        return { article }
+    }
+
+    private getSlug(title: string): string { 
+        return slugify(title, { lower: true }) + "-" + (Math.random() * Math.pow(36, 6) | 0).toString(36)
     }
 }
